f85e3814c236589bda9020552b32dda5a2217e27
Dirk Dobson <dirkvancouver@live.com>
2019-02-28 20:08:57 -0700

    vehicle class

---
+++ Vehicle.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace construtorsAndInheritance
+{
+    public class Vehicle
+    {
+      public Vehicle()
+      {
+        System.Console.WriteLine("Vehicle is being initialized.");
+      }
+    }
+}
